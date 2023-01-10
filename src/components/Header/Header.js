f7666bc14aee0de32e3a2643209b4a3b41ae7d3c
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-09 16:30:24 -0800

    skeleton files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Header.css';
+export default function Footer() {
+  return <header>Header goes here</header>;
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Main.css';
+export default function Footer() {
+  return <main>Main goes here</main>;
+}
