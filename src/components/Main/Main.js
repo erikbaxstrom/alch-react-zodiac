19f38f8a6fa1ecef40e7b8160dfb1476f41a4bb0
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-09 18:04:58 -0800

    add background image

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Main.css';
+import background from '../../background.png';
+
 export default function Main() {
-  return <main>Main goes here</main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      <p>Main goes here</p>
+    </main>
+  );
 }
